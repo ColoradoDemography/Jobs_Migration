a087810d64175b83810ae046cec3e067ba73bfe0
TBleess <todd.bleess@state.co.us>
2021-07-08 17:07:53 -0600

    Adding regions

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 //initialize data
 var bea_url = "https://gis.dola.colorado.gov/lookups/bea_jobs?county="
 var mig_url = "https://gis.dola.colorado.gov/lookups/components?vars=netmigration&year=1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019&county="
+var mig_reg_url = "https://gis.dola.colorado.gov/lookups/components_region?vars=netmigration&year=1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019&reg_num="
 var bea_json = getData(bea_url,0);
 var mig_json = getData(mig_url,0);
 
@@ -59,7 +60,11 @@ selectElem.addEventListener('change', function() {
   chartTitle.text = 'Job Change and Net Migration - ' + countyname;
   mixedChartData.datasets.forEach(function(dataset,k) { 
     bea_json = getData(bea_url,selectElem.value);
-    mig_json = getData(mig_url,selectElem.value);
+    if (selectElem.value < 200){
+      mig_json = getData(mig_url,selectElem.value);
+    } else {
+      mig_json = getData(mig_reg_url,selectElem.value-500);
+    }
     dataset.data = [];
     if (k == 0){
       dataset.data.push(bea_json[0].jobs_1985 - bea_json[0].jobs_1984);
